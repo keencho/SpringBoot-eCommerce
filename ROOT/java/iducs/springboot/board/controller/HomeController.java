bd863e4d5bda62d0b3d5a19ac594971d205c330a
keencho <seyoung050412@gmail.com>
2019-09-14 21:25:08 +0900

    프로젝트 설명서

--- ROOT/java/iducs/springboot/board/controller/HomeController.java
+++ ROOT/java/iducs/springboot/board/controller/HomeController.java
@@ -1,12 +1,22 @@
 package iducs.springboot.board.controller;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URLEncoder;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import iducs.springboot.board.domain.Notice;
 import iducs.springboot.board.domain.OrderInfo;
 import iducs.springboot.board.domain.Product;
 import iducs.springboot.board.domain.ProductReview;
@@ -127,6 +137,46 @@ public class HomeController {
 	@GetMapping("/404")
 	public String error() {		
 		return "home/main/404";
+	}
+	
+	@GetMapping("/project/explain")
+	public void noticeDownload(
+			HttpServletResponse response,
+			HttpServletRequest request
+			) throws Exception{
+		String osName = System.getProperty("os.name");
+		File file;
+		File cwd = new File("src/main/resources/static/uploads");	// 윈도우 업로드 경로
+		File cwd2 = new File("webapps/ROOT/WEB-INF/classes/static/uploads");	// 리눅스 실제 웹서버 업로드 경로
+		
+		if(osName.matches(".*Windows.*")) {
+			file = new File(cwd + "/프로젝트 설명서.docx");
+		} else {
+			file = new File(cwd2 + "/프로젝트 설명서.docx");
+		}
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+
+		String header = request.getHeader("User-Agent");
+		String fileName;
+
+		 if ((header.contains("MSIE")) || (header.contains("Trident")) || (header.contains("Edge"))) {
+	        //인터넷 익스플로러 10이하 버전, 11버전, 엣지에서 인코딩 
+	        fileName = URLEncoder.encode("프로젝트 설명서.docx", "UTF-8");
+		 } else {
+	        //나머지 브라우저에서 인코딩
+	        fileName = new String("프로젝트 설명서.docx".getBytes("UTF-8"), "iso-8859-1");
+	    }
+		 
+		//형식을 모르는 파일첨부용 contentType
+	    response.setContentType("application/octet-stream");
+
+	    //다운로드와 다운로드될 파일이름
+	    response.setHeader("Content-Disposition", "attachment; filename=\""+ fileName + "\"");
+		FileCopyUtils.copy(in, response.getOutputStream());
+	    in.close();
+	    response.getOutputStream().flush();
+	    response.getOutputStream().close();
+
 	}
 
 }
